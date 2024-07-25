876a8255a47e3309ab4df70d707900e51b0be9f0
Gotvacha <vladimir.s.barev.2020@elsys-bg.org>
2024-07-25 03:45:02 +0300

    Initial Commit

---
+++ WordMemorizer/WordMemorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordMemorizerApp.swift
+//  WordMemorizer
+//
+//  Created by Vladiator on 25.07.24.
+//
+
+import SwiftUI
+
+@main
+struct WordMemorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
